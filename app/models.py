275244f1021a0b3e90098f07f81c1aff04b80744
Maxeg228 <64090503+Maxeg228@users.noreply.github.com>
2023-03-12 17:45:00 +0300

    Start

--- app/models.py
+++ app/models.py
@@ -1,17 +1,29 @@
-import psycopg2
-from psycopg2 import Error
-from app import db
+import datetime
+from app import db, app
 
 
 class User(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    def is_exist(self):
+        with app.app_context():
+            return db.session.query(User.username).filter_by(username=self.username).first()
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
 
 
 # try:
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -32,9 +33,11 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User(username=form.username.data, password_hash=form.password.data)
+        if user.is_exist():
+            flash('Login requested for user {}, remember_me={}'.format(
+                form.username.data, form.remember_me.data))
+            return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 
